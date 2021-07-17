5ab55ff01b935f8228ddcb56da58b60bccb54430
jaga <jagadeeshjaga042@gmail.com>
2021-07-17 18:22:17 +0530

    files updated

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,12 @@ resource "aws_vpc" "main" {
     purpose = "jenkins-demo"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "local-subnet"
+  }
+}
